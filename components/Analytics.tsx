a44b7cf3327e98473f976764ca4e8ffa80308808
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-11-14 20:07:21 +0700

    Update example and bump SDK version

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,5 +1,3 @@
-// 'use client';
-
 import React from 'react';
 import {
   CollectionBrowser,
@@ -9,35 +7,36 @@ import {
   MetabaseProvider,
   StaticDashboard,
   StaticQuestion,
+  type SDKConfig,
 } from '@metabase/embedding-sdk-react';
 import { Divider, Title } from '@mantine/core';
 
-const config = {
+const config: SDKConfig = {
   metabaseInstanceUrl: 'http://localhost:3000', // Required: Your Metabase instance URL
-  jwtProviderUri: '/api/sso/metabase', // Required: An endpoint in your app that returns signs the user in and delivers a token
+  authProviderUri: '/api/sso/metabase', // Required: An endpoint in your app that returns signs the user in and delivers a token
 };
 
 export default function Analytics() {
   return (
     <MetabaseProvider config={config}>
       <Title order={2}>{'<InteractiveQuestion />'}</Title>
-      <InteractiveQuestion questionId={2249} />
+      <InteractiveQuestion questionId={1} />
       <Divider my="lg" />
 
       <Title order={2}>{'<StaticQuestion />'}</Title>
-      <StaticQuestion questionId={2249} />
+      <StaticQuestion questionId={1} />
       <Divider my="lg" />
 
       <Title order={2}>{'<InteractiveDashboard />'}</Title>
-      <InteractiveDashboard dashboardId={8} />
+      <InteractiveDashboard dashboardId={1} />
       <Divider my="lg" />
 
       <Title order={2}>{'<StaticDashboard />'}</Title>
-      <StaticDashboard dashboardId={8} />
+      <StaticDashboard dashboardId={1} />
       <Divider my="lg" />
 
       <Title order={2}>{'<EditableDashboard />'}</Title>
-      <EditableDashboard dashboardId={8} />
+      <EditableDashboard dashboardId={1} />
       <Divider my="lg" />
 
       <Title order={2}>{'<CollectionBrowser />'}</Title>
